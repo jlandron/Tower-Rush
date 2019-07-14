439f5ababddc49ac9fccc36c7e9c775b157d1392
Josh Landron <jlandron@uw.edu>
2019-07-14 00:37:28 -0700

    Made grid snapping for map creation

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [Range(1, 20)][SerializeField] int gridSize = 10;
+    void Update( ) {
+        Vector3 snapPosition;
+        snapPosition.x = Mathf.RoundToInt( transform.position.x / gridSize ) * gridSize;
+        snapPosition.y = Mathf.RoundToInt( transform.position.y / gridSize ) * gridSize;
+        snapPosition.z = Mathf.RoundToInt( transform.position.z / gridSize ) * gridSize;
+        transform.position = snapPosition;
+
+    }
+
+}
