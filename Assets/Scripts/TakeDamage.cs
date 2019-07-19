0cfc9ea00c0bf4453014116a14987453a141e17d
Josh Landron <jlandron@uw.edu>
2019-07-19 00:24:49 -0700

    Added Particle effects and hitpoints

---
+++ Assets/Scripts/TakeDamage.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TakeDamage : MonoBehaviour {
+
+    [SerializeField] int hitPoints = 5;
+    void OnParticleCollision( GameObject other ) {
+        hitPoints--;
+        if( hitPoints <= 0 ) {
+            Destroy( this.gameObject );
+        }
+    }
+}
