d3d5a6087bef4334df34abf0d60fd0c0c369169d
Josh Landron <jlandron@uw.edu>
2019-07-19 20:21:38 -0700

    Added spawning script

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] float secondsBetweenSpawns = 2;
+    [SerializeField] float numberOfEnemies = 10;
+    [SerializeField] EnemyMover enemyPrefab;
+    // Start is called before the first frame update
+    void Start( ) {
+        //start co-routine
+        StartCoroutine( SpawnEnemies( ) );
+    }
+    IEnumerator<WaitForSeconds> SpawnEnemies( ) {
+        for( int i = 0; i < numberOfEnemies; i++ ) {
+            print( "Spawn enamies" );
+            
+            yield return new WaitForSeconds( secondsBetweenSpawns );
+        }
+
+    }
+}
